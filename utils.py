95f6f7ac4a98e61b7402e9b31db828a090bd7966
Pranav Tiwari <pranavtiwari@Pranavs-MBP.Dlink>
2019-12-15 16:51:04 +0530

    added method to overlay text on a video

---
+++ utils.py
@@ -0,0 +1,10 @@
+from moviepy.editor import TextClip, CompositeVideoClip, VideoFileClip
+
+
+def add_text_to_video(file_path, text):
+    text_clip = TextClip(text, color='white', font="Amiri-Bold", fontsize=100).set_pos("center")
+    clip = VideoFileClip(file_path)
+    text_clip.duration = clip.duration
+    final_clip = CompositeVideoClip([clip, text_clip])
+    final_clip.duration = clip.duration
+    final_clip.write_videofile("videos/final_video.mp4")
